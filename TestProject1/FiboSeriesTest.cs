ec0c4307410eb5a7f6c8bc312822a0a1ab58ba75
pouyatirdad <pouyatirdad70@gmail.com>
2022-02-06 20:53:17 +0330

    add fixture class

--- TestProject1/FiboSeriesTest.cs
+++ TestProject1/FiboSeriesTest.cs
@@ -4,13 +4,23 @@ using Xunit;
 
 namespace TestProject1
 {
-    public class FiboSeriesTest
+    public class FiboSeriesFixture
     {
+        public Calculator Calc => new Calculator();
+    }
+    public class FiboSeriesTest : IClassFixture<FiboSeriesFixture>
+    {
+        private readonly FiboSeriesFixture _fiboSeriesFixture;
+        public FiboSeriesTest(FiboSeriesFixture fiboSeriesFixture)
+        {
+            _fiboSeriesFixture = fiboSeriesFixture;
+        }
+
         [Fact]
         [Trait("Category", "FiboSeries")]
         public void FiboSeriesNotContainsZero()
         {
-            var fibo = new Calculator();
+            var fibo = _fiboSeriesFixture.Calc;
             var result = fibo.FiboSeries;
             Assert.All(result, n => Assert.NotEqual(0, n));
             //another way
@@ -20,17 +30,17 @@ namespace TestProject1
         [Trait("Category", "FiboSeries")]
         public void FiboSeriesContains13()
         {
-            var fibo = new Calculator();
+            var fibo = _fiboSeriesFixture.Calc;
             var result = fibo.FiboSeries;
             Assert.Contains(13, result);
         }
         [Fact]
-        [Trait("Category","FiboSeries")]
+        [Trait("Category", "FiboSeries")]
         public void CheckFiboList()
         {
-            var expectedFiboSeriesList = new List<int>() {1,1,2,3,5,8,13};
-            var result = new Calculator().FiboSeries;
-            Assert.Equal(expectedFiboSeriesList,result);
+            var expectedFiboSeriesList = new List<int>() { 1, 1, 2, 3, 5, 8, 13 };
+            var result = _fiboSeriesFixture.Calc.FiboSeries;
+            Assert.Equal(expectedFiboSeriesList, result);
         }
     }
 }
