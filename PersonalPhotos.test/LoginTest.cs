fd24e883a224b217eb147a773eeb35f9a3bb63d7
pouyatirdad <pouyatirdad70@gmail.com>
2022-02-10 16:29:58 +0330

    add test with mock for invalid model state

--- PersonalPhotos.test/LoginTest.cs
+++ PersonalPhotos.test/LoginTest.cs
@@ -4,6 +4,8 @@ using PersonalPhotos.Controllers;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PersonalPhotos.Models;
+using System.Threading.Tasks;
 
 namespace PersonalPhotos.test
 {
@@ -19,6 +21,7 @@ namespace PersonalPhotos.test
             _accessor = new Mock<IHttpContextAccessor>();
             _controller = new LoginsController(_logins.Object, _accessor.Object);
         }
+        [Fact]
         public void Index_GivenNoReturnUrl_ReturnLoginView()
         {
             var result = (_controller.Index() as ViewResult);
@@ -31,6 +34,14 @@ namespace PersonalPhotos.test
 
             // these code can use on two test class
             Assert.NotNull(result);
+            Assert.Equal("Login",result.ViewName,ignoreCase:true);
+        }
+        [Fact]
+        public async Task Login_GivenModelStateInvalid_ReturnLoginView()
+        {
+            _controller.ModelState.AddModelError("Test","Test");
+
+            var result = await _controller.Login(Mock.Of<LoginViewModel>()) as ViewResult;
             Assert.Equal("Login",result.ViewName,ignoreCase:true);
         }
     }
